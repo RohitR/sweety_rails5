3038928f24fad3bae23620a9c66ef6b30feccff1
rohitdnx <2255rohith@gmail.com>
2016-08-11 01:40:51 +0530

    action cable allow requst from async

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Sweety
   class Application < Rails::Application
-    config.autoload_paths << Rails.root.join('lib')
+    config.eager_load_paths << Rails.root.join('lib')
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the Rails application.
 require_relative 'application'
+Rails.application.config.action_cable.disable_request_forgery_protection = true
 
 # Initialize the Rails application.
 Rails.application.initialize!
